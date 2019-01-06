57d39301fc2373f7b929c967de43555b899d6a03
Pattio <merfisku@gmail.com>
2019-01-06 18:25:42 +0200

    Add method to complete the path

--- deepswarm/graph.py
+++ deepswarm/graph.py
@@ -2,7 +2,8 @@
 # Licensed under MIT License
 
 import random
-from .nodes import InputNode
+from .nodes import (Conv2DNode, DenseNode, DropoutNode, EndNode, FlattenNode,
+                    InputNode, MaxPool2DNode)
 
 
 class Graph:
@@ -20,4 +21,16 @@ class Graph:
             current_node = current_node.neighbours[random_index]
             # Add only copy of the node, so that original stays unmodified
             path.append(current_node.create_deepcopy())
+        completed_path = self.complete_path(path)
+        return completed_path
+
+    def complete_path(self, path):
+        # If path is already completed then return that path
+        if type(path[-1]) is EndNode:
+            return path
+        # Otherwise complete the path and then return completed path
+        if type(path[-1]) in (Conv2DNode, MaxPool2DNode):
+            path.append(FlattenNode())
+        if type(path[-1]) in (FlattenNode, DenseNode, DropoutNode):
+            path.append(EndNode())
         return path
