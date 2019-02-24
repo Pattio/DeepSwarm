3a7793b9a9f5b3ba54af0d1f629730fd5167eab7
Pattio <merfisku@gmail.com>
2019-02-24 14:24:38 +0000

    Add fashion MNIST example

--- examples/fashion-mnist.py
+++ examples/fashion-mnist.py
@@ -7,9 +7,9 @@ import tensorflow as tf
 from deepswarm.backends import Dataset, TFKerasBackend
 from deepswarm.deepswarm import DeepSwarm
 
-# Load MNIST dataset
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# Load Fashion MNIST dataset
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 # Normalize and reshape data
 x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
